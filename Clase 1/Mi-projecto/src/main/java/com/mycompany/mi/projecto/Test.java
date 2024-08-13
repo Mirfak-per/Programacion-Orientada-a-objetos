88df5e15341746091c9be446d6f004e64e56fc80
Mirfak-per <ga.hernandezl@duocuc.cl>
2024-08-13 13:42:46 -0400

    Add files via upload
    Primera Clase

---
+++ Clase 1/Mi-projecto/src/main/java/com/mycompany/mi/projecto/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mi.projecto;
+
+/**
+ *
+ * @author PAAPCA02LC0806
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
